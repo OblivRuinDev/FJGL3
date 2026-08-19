6f0e2977a6b35df4715a50776d4d5e6903d507d9
Ioannis Tsakpinis <iotsakp@gmail.com>
2026-08-19 19:12:57 +0300

    feat(harfbuzz) update to 14.3.1

--- modules/lwjgl/harfbuzz/src/generated/java/org/lwjgl/util/harfbuzz/HarfBuzz.java
+++ modules/lwjgl/harfbuzz/src/generated/java/org/lwjgl/util/harfbuzz/HarfBuzz.java
@@ -902,9 +902,9 @@ public class HarfBuzz {
 
     public static final int HB_VERSION_MINOR = 3;
 
-    public static final int HB_VERSION_MICRO = 0;
+    public static final int HB_VERSION_MICRO = 1;
 
-    public static final String HB_VERSION_STRING = "14.3.0";
+    public static final String HB_VERSION_STRING = "14.3.1";
 
     protected HarfBuzz() {
         throw new UnsupportedOperationException();
--- modules/lwjgl/harfbuzz/src/templates/kotlin/harfbuzz/templates/HarfBuzz.kt
+++ modules/lwjgl/harfbuzz/src/templates/kotlin/harfbuzz/templates/HarfBuzz.kt
@@ -3864,9 +3864,9 @@ val hb = "HarfBuzz".nativeClass(Module.HARFBUZZ, prefix = "HB", prefixMethod = "
 
     IntConstant("VERSION_MAJOR".."14")
     IntConstant("VERSION_MINOR".."3")
-    IntConstant("VERSION_MICRO".."0")
+    IntConstant("VERSION_MICRO".."1")
 
-    StringConstant("VERSION_STRING".."14.3.0")
+    StringConstant("VERSION_STRING".."14.3.1")
 
     customMethod("""
     public static boolean HB_VERSION_ATLEAST(int major, int minor, int micro) {
